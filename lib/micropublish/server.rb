7a4d59b2d868b802354c56309ad7e57bf613109d
public@barryfrost.com <public@barryfrost.com>
2015-02-05 09:33:37 +0000

    Refactor label/icon/fields setup and display.

--- lib/micropublish/server.rb
+++ lib/micropublish/server.rb
@@ -1,20 +1,20 @@
 module Micropublish
   class Server < Sinatra::Base
-  
+
     configure do
       set :views, "#{File.dirname(__FILE__)}/../../views"
 
       # use a cookie that lasts for 30 days
       use Rack::Session::Cookie, secret: ENV['COOKIE_SECRET'], expire_after: 2592000
     end
-  
+
     before do
       if logged_in?
         @bookmark_url = "#{request.base_url}/?micropub_endpoint=#{CGI::escape(session[:micropub_endpoint])}"
-        @bookmark_url += "&token=#{CGI::escape(session[:token])}" 
+        @bookmark_url += "&token=#{CGI::escape(session[:token])}"
       end
     end
-  
+
     helpers do
       def h(text)
         Rack::Utils.escape_html(text)
@@ -31,7 +31,7 @@ module Micropublish
       end
       erb :index
     end
-  
+
     get '/auth' do
       # define random state string
       session[:state] = Auth.generate_state
@@ -48,7 +48,7 @@ module Micropublish
       query = URI.encode_www_form(hash)
       redirect "#{endpoints[:authorization_endpoint]}?#{query}"
     end
-  
+
     get '/auth/callback' do
       puts "params=#{params.inspect}"
       endpoints_and_token = Auth.callback(params[:me], params[:code], session[:state], "#{request.base_url}/auth/callback")
@@ -57,20 +57,17 @@ module Micropublish
       session.merge!(endpoints_and_token)
       redirect :new
     end
-  
+
     post '/logout' do
       logout!
     end
-  
+
     get %r{^/new/?(note|article|bookmark|reply|repost|like)?/?$} do |post_type|
       require_session
-      @post_type = post_type || 'note'
-      @fields = post_type_fields(post_type)
-      @post_types = %w(note article bookmark reply repost like)
-      @post_type_icons = %w(comment file-text bookmark reply retweet heart)
+      @post_type = (post_type || 'note').to_sym
       erb :new
     end
-  
+
     post '/create' do
       require_session
       post_url = Micropub.post(session[:micropub_endpoint], params, session[:token])
@@ -84,31 +81,25 @@ module Micropublish
     def require_session
       redirect '/' unless logged_in?
     end
-  
+
     def logout!
       session.clear
       redirect '/'
     end
-  
+
     def logged_in?
       session.key?(:micropub_endpoint) && session.key?(:token)
     end
-    
-    def post_type_fields(post_type)
-      case post_type
-      when 'article'
-        %i(name content category)
-      when 'bookmark'
-        %i(bookmark content category)
-      when 'reply'
-        %i(in_reply_to content category)
-      when 'repost'
-        %i(repost_of category)
-      when 'like'
-        %i(like_of category)
-      else
-        %i(content category)
-      end
+
+    def post_types
+      {
+        note:     { label: 'Note',     icon: 'comment',   fields: %i(content category) },
+        article:  { label: 'Article',  icon: 'file-text', fields: %i(name content category) },
+        bookmark: { label: 'Bookmark', icon: 'bookmark',  fields: %i(bookmark content category) },
+        reply:    { label: 'Reply',    icon: 'reply',     fields: %i(in_reply_to content category) },
+        repost:   { label: 'Repost',   icon: 'retweet',   fields: %i(repost_of category) },
+        like:     { label: 'Like',     icon: 'heart',     fields: %i(like_of category) }
+      }
     end
 
   end
