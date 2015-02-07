fd55aa0bd88efeb7902f70f156913dc29c8de517
public@barryfrost.com <public@barryfrost.com>
2015-02-07 18:51:27 +0000

    Handle case when there's not a reply parameter on the reply page.

--- lib/micropublish/server.rb
+++ lib/micropublish/server.rb
@@ -58,7 +58,7 @@ module Micropublish
 
     get %r{^/new/?(note|article|bookmark|reply|repost|like)?/?$} do |post_type|
       require_session
-      if post_type == 'reply'
+      if post_type == 'reply' && params.key?('in-reply-to')
         reply_username = Micropub.reply_username(params['in_reply_to'])
         params['content'] ||= "@#{reply_username} " unless reply_username.nil?
       end
